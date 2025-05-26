67ed6e66daa32989bd07044ef70fc37c6362d77e
juseojo <j86371224@gmail.com>
2025-05-26 13:03:33 +0900

    Feat: 필수 구현 기능들 완성

--- number_baseBall/main.swift
+++ number_baseBall/main.swift
@@ -7,5 +7,95 @@
 
 import Foundation
 
-print("Hello, World!")
+print("<게임을 시작합니다.")
 
+var isEndGame: Bool = false
+var answerNum = Int.random(in: 1...9) * 100 + Int.random(in: 1...9) * 10 + Int.random(in: 1...9)
+var answerString = String(answerNum)
+
+while isEndGame == false {
+	print("숫자를 입력하세요")
+	guard let inputNumberString = readLine(), let inputNumber = Int(inputNumberString) else {
+		print("올바르지 않은 입력값입니다")
+		continue
+	}
+	
+	guard checkQuestion(tryNum: inputNumber, tryString: inputNumberString) else {
+		print("올바르지 않은 입력값입니다")
+		continue
+	}
+
+	if checkAnswer(answerString: answerString, tryString: inputNumberString) {
+		isEndGame = true
+	}
+}
+
+func checkQuestion(tryNum: Int, tryString: String) -> Bool{
+	// 자리수 확인
+	if tryNum > 999  || tryNum < 100{
+		return false
+	}
+
+	// 중복확인
+	var count = 0
+	for i in tryString {
+		for j in tryString {
+			if i == j {
+				count += 1
+			}
+		}
+
+		if count > 1 {
+			return false
+		} else {
+			count = 0
+		}
+	}
+
+	// 0 사용 확인
+	for c in tryString {
+		if c == "0" as Character {
+			return false
+		}
+	}
+
+	return true
+}
+
+func checkAnswer(answerString: String, tryString: String) -> Bool {
+
+	var strikeCount = 0
+	var ballCount = 0
+	var count = 0
+
+	if answerString == tryString {
+		print("정답입니다!")
+
+		return true
+	}
+
+	// 같은 위치에 같은 요소면 스트라이크, 같은 위치 아닌데 같은 요소면 볼
+	for c in tryString {
+		for i in 0..<answerString.count {
+			if answerString.getChar(at: i) == c {
+				if count == i {
+					strikeCount += 1
+				} else {
+					ballCount += 1
+				}
+			}
+		}
+
+		count += 1
+	}
+
+	print("\(strikeCount)스트라이크 \(ballCount)볼")
+
+	return false
+}
+
+extension String {
+	func getChar(at index: Int) -> Character {
+		return self[self.index(self.startIndex, offsetBy: index)]
+	}
+}
